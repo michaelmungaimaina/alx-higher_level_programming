a86b3f7132e51495699cedd6c24b5c102369d0ef
maina michael <pantalemonmongah@gmail.com>
2024-01-10 16:36:48 +0300

    Add file 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    """
+    A function that replaces or adds
+    key/value in a dictionary.
+    """
+    my_dict[key] = value
+    return my_dict
