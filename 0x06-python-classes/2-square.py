25b47adc5585c39ec3d0af7cf5fc1f5f74189e71
maina michael <pantalemonmongah@gmail.com>
2024-01-24 21:49:08 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
 
 """
+Defines a blueprint for a Square as part of the OOP tasks
+"""
+
+
 class Square:
+    """
+    A blueprint for working with squares
+    """
 
-    def __init__(self, size=0):
-        """__init__
+    def __init__(self, size: int = 0):
+        """
+        Initializes the Square class
 
-        The __init__ method initializes the size value of the square.
 
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
+        Args:
+            size (int, optional): the size of the square. Defaults to 0.
 
         Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
+            TypeError: when the size received is not an integer
+            ValueError: when the size if not a positive integer
         """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
+        # size must be an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        # it should be greater than or equal to zero
         if size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
 
+        # set the size of the Square if all goes well
         self.__size = size
