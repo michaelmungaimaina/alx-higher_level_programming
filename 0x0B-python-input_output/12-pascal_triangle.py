aead609eddd6d630221d9f422cfcef9234681233
maina michael <pantalemonmongah@gmail.com>
2024-02-07 13:15:49 +0300

    Code error fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,17 +10,27 @@ def pascal_triangle(n):
     Returns:
         list: A list of lists representing Pascal's triangle.
     """
-    if n <= 0:
+
+    f n <= 0:
         return []
 
+    limit = n - 1
     triangle = [[1]]
 
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    for i in range(limit):
+        row = []
+        row.append(1)
+
+        if len(triangle[i]) > 1:
+            prev_row_len = len(triangle[i]) - 1
+            nxt = 1
+
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
+                nxt += 1
+
         row.append(1)
         triangle.append(row)
 
     return triangle
-
